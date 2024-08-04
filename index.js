693ce3e79996b9d5a97530af96040398081dfa7d
Victorsam862 <150631843+Victorsam862@users.noreply.github.com>
2024-08-05 00:53:09 +0400

    first deploy

---
+++ index.js
@@ -0,0 +1,41 @@
+const header = document.getElementById('header');
+
+window.addEventListener('scroll', () => {
+  if (window.scrollY > 100) {
+    header.classList.add('scrolled');
+  } else {
+    header.classList.remove('scrolled');
+  }
+});
+
+const headers = document.getElementById('headers');
+
+window.addEventListener('scroll', () => {
+  if (window.scrollY > 100) {
+    headers.classList.add('scrolled');
+  } else {
+    headers.classList.remove('scrolled');
+  }
+});
+
+function support(){
+    alert('9LAjk5F4rFetELE4CygcBbZ5hYc2QhRrbJjfm5Q26jWM')
+}
+
+
+
+
+
+const menuToggle = document.getElementById('menu-toggle');
+const menu = document.getElementById('menu');
+const closeBtn = document.querySelector('.close-btn');
+
+menuToggle.addEventListener('click', function() {
+  menuToggle.classList.toggle('active');
+  menu.classList.toggle('active');
+});
+
+closeBtn.addEventListener('click', function() {
+  menuToggle.classList.remove('active');
+  menu.classList.remove('active');
+});
